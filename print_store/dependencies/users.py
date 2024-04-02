15c707321ed785a9cb397eb0c3a0fe4b7119d5ec
Эмилар Локтев <emilar-l@yandex.ru>
2024-04-02 22:35:21 +0300

    Add is_authenticated dependency

---
+++ print_store/dependencies/users.py
@@ -0,0 +1,23 @@
+from typing import Annotated
+
+from fastapi import Header
+from jose import jwt, JWTError
+
+from config import TOKEN_TYPE, SECRET_KEY, ALGORITHM
+from extra.http_exceptions import AuthError
+from extra.services import get_instance_or_404
+from models.users import User
+
+
+async def is_authenticated(Authorization: Annotated[str, Header()]) -> str:
+    token_type, token = Authorization.split()
+    if not token_type == TOKEN_TYPE:
+        raise AuthError
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        if not username:
+            raise AuthError
+    except JWTError:
+        raise AuthError
+    return await get_instance_or_404(User, username=username)
--- print_store/routers/users.py
+++ print_store/routers/users.py
@@ -1,17 +1,16 @@
 import os
 from typing import Annotated
 
-from fastapi import APIRouter, Header
+from fastapi import APIRouter, Depends
 from tortoise.exceptions import IntegrityError
-from jose import jwt, JWTError
 
 from models.users import User
 from py_models.users import PyUserModelIn, PyUserModeOut
 from extra.utils import get_password_hash
-from config import ALGORITHM, SECRET_KEY, TOKEN_TYPE
 from routers import auth
-from extra.http_exceptions import AuthError, UserAlreadyExistsError
-from extra.services import get_instance_or_404, get_instance, create_instance
+from extra.http_exceptions import UserAlreadyExistsError
+from extra.services import get_instance, create_instance
+from dependencies.users import is_authenticated
 
 
 router = APIRouter(prefix='/users')
@@ -39,16 +38,6 @@ async def create_user(user: PyUserModelIn) -> PyUserModeOut:
 
 @router.get('/me')
 async def get_current_user(
-    Authorization: Annotated[str, Header()]
+    user: Annotated[str, Depends(is_authenticated)],
 ) -> PyUserModeOut:
-    token_type, token = Authorization.split()
-    if not token_type == TOKEN_TYPE:
-        raise AuthError
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get('sub')
-        if not username:
-            raise AuthError
-    except JWTError:
-        raise AuthError
-    return await get_instance_or_404(User, username=username)
+    return user
